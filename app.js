74cd8fcb9b03871ddcb9e88ca34a0f94334d0cb7
YodJI <yodji62@yandex.ru>
2021-02-17 21:39:42 +0300

    Prettier on

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const path = require('path');
-const userRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const express = require("express");
+const path = require("path");
+const userRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
 
 const app = express();
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', userRouter);
-app.use('/', cardsRouter);
+app.use("/", userRouter);
+app.use("/", cardsRouter);
 
-app.use((req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+app.use((req, res) =>
+  res.status(404).send({ message: "Запрашиваемый ресурс не найден" })
+);
 
 app.listen(PORT);
--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,14 @@
-const router = require('express').Router();
-const path = require('path');
-const getDataFromFile = require('../helpers/getFromFile');
+const router = require("express").Router();
+const path = require("path");
+const getDataFromFile = require("../helpers/getFromFile");
 
-const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
+const dataPath = path.join(__dirname, "..", "data", "cards.json");
 
-const getCards = (req, res) => getDataFromFile(dataPath)
-  .then((cards) => res.status(200).send(cards))
-  .catch((err) => res.status(400).send(err));
+const getCards = (req, res) =>
+  getDataFromFile(dataPath)
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) => res.status(400).send(err));
 
-router.get('/cards', getCards);
+router.get("/cards", getCards);
 
 module.exports = router;
