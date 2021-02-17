c958d7c6daeacf5a330cb2661a826e87b3eb7941
YodJI <yodji62@yandex.ru>
2021-02-17 21:31:33 +0300

    Исправил ошибки линтинга. Убрал лишний catch в файле для чтения файлов

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const path = require('path');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', userRouter)
+app.use('/', userRouter);
 app.use('/', cardsRouter);
 
-app.use((req, res) => {
-  return res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
+app.use((req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 app.listen(PORT);
--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
 const path = require('path');
 const getDataFromFile = require('../helpers/getFromFile');
-const { get } = require('./users');
+
 const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
 
-const getCards = (req, res) => {
-  return getDataFromFile(dataPath)
-    .then(cards => res.status(200).send(cards))
-    .catch(err => res.status(400).send(err));
-}
+const getCards = (req, res) => getDataFromFile(dataPath)
+  .then((cards) => res.status(200).send(cards))
+  .catch((err) => res.status(400).send(err));
 
 router.get('/cards', getCards);
 
